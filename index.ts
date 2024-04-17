daa6a57628bb3b204ada8855e4e1753435b1c9d7
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-04-16 22:41:47 -0400

    0.0.9

--- index.ts
+++ index.ts
@@ -2,4 +2,10 @@ import {def} from 'trans-render/lib/def.js';
 import {FetchFor} from './fetch-for.js';
 
 await FetchFor.bootUp();
-def(FetchFor.config.name!, FetchFor);
+def('fetch-for', FetchFor);
+
+declare global {
+    interface HTMLElementTagNameMap {
+      'fetch-for': FetchFor;
+    }
+  }
