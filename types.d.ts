16de22041cf8bd8d01dfd05cbe55f04a8a7fcbe5
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-06-21 18:43:25 -0400

    0.0.10

--- types.d.ts
+++ types.d.ts
@@ -85,7 +85,7 @@ export interface EndUserProps{
 }
 
 export interface OverridableGetters{
-    request$: RequestInit,
+    //request$: RequestInit,
     //accept$?: string,
     value?: any,
     //isAttrParsed?: boolean,
