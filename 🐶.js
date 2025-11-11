23db92f3c2ddcf1a604c7f987a590bfa097d988f
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-11-11 06:51:50 -0500

    0.0.11

---
+++ 🐶.js
@@ -0,0 +1,15 @@
+// @ts-check
+import { MountObserver, seed, BeHive } from 'be-hive/be-hive.js';
+import { emc as baseEMC } from './emc.js';
+/** @import {EMC} from './ts-refs/trans-render/be/types.d.ts' */
+
+/**
+ * @type {EMC}
+ */
+export const emc = {
+    ...baseEMC,
+    base: '🐶',
+    enhPropKey: '🐶',
+};
+const mose = seed(emc);
+MountObserver.synthesize(document, BeHive, mose);
